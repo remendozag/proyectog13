64474afdcac60e910e5e6700554793d73f4d3c7f
remendozag <6662609+remendozag@users.noreply.github.com>
2021-11-19 07:57:34 -0500

    1.0
    Proyecto G13 v1.0

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+const routes: Routes = [
+  {
+    path:"header",
+    component:HeaderComponent
+  },
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"footer",
+    component:FooterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
